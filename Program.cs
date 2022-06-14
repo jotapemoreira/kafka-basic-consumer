07ea48ad514c5212daa885a6764ca243d3d6107f
João Paulo Rodrigues Moreira <jotapemoreira@gmail.com>
2022-06-14 14:33:40 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using Confluent.Kafka;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace kafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Consumir();
+        }
+
+        private static void Consumir()
+        {
+            string bootstrapServers = "localhost:9092";
+            string nomeTopic = "topic.teste";
+            string groupId = "group.teste";
+
+            var logger = new LoggerConfiguration().WriteTo.Console(theme: AnsiConsoleTheme.Literate).CreateLogger();
+            logger.Information("Consumindo mensagem com Kafka");
+
+            logger.Information($"BootstrapServers = {bootstrapServers}");
+            logger.Information($"Topic = {nomeTopic}");
+            logger.Information($"Group Id = {groupId}");
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServers,
+                GroupId = groupId,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+                {
+                    consumer.Subscribe(nomeTopic);
+
+                    try
+                    {
+                        while (true)
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            logger.Information($"Mensagem lida: {cr.Message.Value}");
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        consumer.Close();
+                        logger.Warning("Execução cancelada.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Exceção: {ex.GetType().FullName} | Mensagem: {ex.Message}");
+            }
+        }
+
+    }
+}
